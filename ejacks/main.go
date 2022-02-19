a83fb186f2e07a98fc76367093ad6dfe7513df13
jackmarsh <jackjack97@hotmail.co.uk>
2022-02-19 12:59:11 +0000

    added godit base

--- ejacks/main.go
+++ ejacks/main.go
@@ -2,8 +2,16 @@ package main
 
 import(
 	"fmt"
+	
+	"github.com/gdamore/tcell/termbox"
 )
 
 func main() {
      fmt.Println("welcome to ejacks")
+     err := termbox.Init()
+     if err != nil {
+         panic(err)
+     }
+     defer termbox.Close()
+     termbox.SetInputMode(termbox.InputAlt)
 }
