f864c389b05f27817eb1cb8055345b2a8d1b5ec8
jackmarsh <jackjack97@hotmail.co.uk>
2023-01-05 23:55:12 +0000

    third party fixed

--- ejacks/main.go
+++ ejacks/main.go
@@ -2,16 +2,25 @@ package main
 
 import(
 	"fmt"
+	"time"
 	
-	"github.com/gdamore/tcell/termbox"
+	"github.com/gdamore/tcell/v2"
 )
 
 func main() {
-     fmt.Println("welcome to ejacks")
-     err := termbox.Init()
-     if err != nil {
-         panic(err)
-     }
-     defer termbox.Close()
-     termbox.SetInputMode(termbox.InputAlt)
+	fmt.Println("welcome to ejacks")
+
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		panic(err)
+	}
+	if err := screen.Init(); err != nil {
+		panic(err)
+	}
+	defer screen.Fini()
+	time.Sleep(2 * time.Second)
+
+	screen.EnablePaste()
+	editor := editor.NewEditor(screen)
+	
 }
